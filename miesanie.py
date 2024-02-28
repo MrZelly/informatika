30af821b2bdbc75717e4655bde4e9ccf6db7d044
Your Name <jurko.minaric@gmail.com>
2024-02-28 14:26:49 +0100

    miesanie

--- miesanie.py
+++ miesanie.py
@@ -1,10 +1,13 @@
-import tkinter
+import tkinter, random
 canvas = tkinter.Canvas(width = 880, height = 500, bg = "white")
 canvas.pack()
 
 x = 55
 y = 250
 r = 50
+
+cislo = random.randint(1, 8)
+canvas.create_oval()
 
 for i in range(1,9):
     canvas.create_oval(x-r, y-r, x+r, y+r, fill = "red")
