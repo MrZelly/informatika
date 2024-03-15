59021dd40ff5481237936e3f7610219f0919befe
Your Name <jurko.minaric@gmail.com>
2024-03-15 08:25:23 +0100

    sach

--- skola/sach/sach.py
+++ skola/sach/sach.py
@@ -3,8 +3,18 @@ canvas = tkinter.Canvas(width = 800,height = 800)
 canvas.pack()
 
 meno = ''
+cierne = False
 obrazky = []
 print(obrazky)
+
+for i in range(8):
+    for j in range(8):
+        if cierne:
+            canvas.create_rectangle(j*100, i*100, j*100 + 100, i*100 + 100, fill = "brown", width = 0)
+        else: 
+            canvas.create_rectangle(j*100, i*100, j*100 + 100, i*100 + 100, fill = "white", width = 0)
+        cierne = not cierne
+    cierne = not cierne
 
 for i in range(101,113):
     meno = str(i)+'.png'
@@ -20,3 +30,6 @@ for i in range(101,113):
     
     canvas.create_image(x,y, image = obrazky[i-101])
 
+
+
+canvas.mainloop()
