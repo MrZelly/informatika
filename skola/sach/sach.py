ddb7f36fa05be4be207ddbf4074ae475eea12202
Your Name <jurko.minaric@gmail.com>
2024-05-22 13:45:26 +0200

    sach wip

--- skola/sach/sach.py
+++ skola/sach/sach.py
@@ -4,7 +4,16 @@ canvas.pack()
 
 meno = ''
 obrazky = []
-
+sachovnica = [
+    ["VezaC1", "KonC1", "StrelecC1", "KralovnaC", "KralC", "StrelecC1", "KonC1", "VezaC1"],
+    ["PesiakC1", "PesiakC2", "PesiakC3", "PesiakC4", "PesiakC5", "PesiakC6", "PesiakC7", "PesiakC8"],
+    ["X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X"],
+    ["X", "X", "X", "X", "X", "X", "X", "X"],
+    ["PesiakB1", "PesiakB2", "PesiakB3", "PesiakB4", "PesiakB5", "PesiakB6", "PesiakB7", "PesiakB8"],
+    ["VezaB1", "KonB1", "StrelecB1", "KralovnaB", "KralB", "StrelecB1", "KonB1", "VezaB1"]
+]
 biele = True # na zmenu farby
 
 for i in range(8):     #stlpce
@@ -66,55 +75,127 @@ origposy = 0
 bpesiak = [0] * 8
 cpesiak = [0] * 8
 
+bol_vyber = False
+biely = True
+
+def check_posun(new_x, new_y):
+    global sachovnica
+    if origposy < new_y:
+        for posun_y in range(origposy, new_y):
+            print(posun_y)
+            if sachovnica[posun_y][new_x] != "X":
+                print("lol")
+                return True
+    else:
+        for posun_y in range(new_y, origposy):
+            print(posun_y)
+            if sachovnica[posun_y][new_x] != "X":
+                print("lol")
+                return True
+    print(sachovnica)
+    print(origposx)
+    print(origposy)
+    print(new_x)
+    print(new_y)
+    sachovnica[new_y][new_x], sachovnica[origposy][origposx] = sachovnica[origposy][origposx], sachovnica[new_y][new_x]
+    print(sachovnica)
+    return False
+
 def posun(event, co):
-    global x1, y1, figurka, origposx, origposy
+    global x1, y1, figurka, origposx, origposy, bol_vyber, biely
     x1, y1 = event.x, event.y
-    if co == "vyber":        
-        figurka = canvas.gettags("current")[0]
-        origposx = x1 // 100
-        origposy = y1 // 100
+    if co == "vyber":
+        bol_vyber = not bol_vyber
+        if bol_vyber:
+            figurka = canvas.gettags("current")[0]
+            canvas.itemconfig("current", fill = "red")
+            origposx = x1 // 100
+            origposy = y1 // 100
+        else:
+            figurka_2 = canvas.gettags("current")[0]
+            origposx_2 = x1 // 100
+            origposy_2 = y1 // 100
+            if figurka.startswith("pesiak"):
+                if figurka[6] == "B" and ((origposx_2 == origposx + 1 and origposy_2 == origposy - 1) or (origposx_2 == origposx - 1 and origposy_2 == origposy - 1)):
+                    if "C" in figurka_2 and biely:
+                        x1 = x1 // 100 * 100 + 50
+                        y1 = y1 // 100 * 100 + 50
+                        canvas.coords(figurka, x1, y1)
+                        canvas.delete(figurka_2)
+                        bol_vyber = False
+                if figurka[6] == "C" and ((origposx_2 == origposx + 1 and origposy_2 == origposy + 1) or (origposx_2 == origposx - 1 and origposy_2 == origposy + 1)):
+                    if "B" in figurka_2 and not biely:
+                        x1 = x1 // 100 * 100 + 50
+                        y1 = y1 // 100 * 100 + 50
+                        canvas.coords(figurka, x1, y1)
+                        canvas.delete(figurka_2)
+                        bol_vyber = False
+            if figurka.startswith("veza"):
+                if (origposx != origposx_2 and origposy == origposy_2) or (origposx == origposx_2 and origposy != origposy_2):
+                    if "C" in figurka_2 and biely:
+                        x1 = x1 // 100 * 100 + 50
+                        y1 = y1 // 100 * 100 + 50
+                        canvas.coords(figurka, x1, y1)
+                        canvas.delete(figurka_2)
+                        bol_vyber = False
+                    if "B" in figurka_2 and not biely:
+                        x1 = x1 // 100 * 100 + 50
+                        y1 = y1 // 100 * 100 + 50
+                        canvas.coords(figurka, x1, y1)
+                        canvas.delete(figurka_2)
+                        bol_vyber = False
+            if figurka.startswith("strelec"):
+                if (abs(origposy_2 - origposy) == abs(origposx_2 - origposx)) and origposy_2 != origposy:
+                    x1 = x1 // 100 * 100 + 50
+                    y1 = y1 // 100 * 100 + 50
+                    canvas.coords(figurka, x1, y1)
+                    canvas.delete(figurka_2)
+            biely = not biely
     if co == "posun":
         polickox = x1 // 100
         polickoy = y1 // 100
         x1 = x1 // 100 * 100 + 50
         y1 = y1 // 100 * 100 + 50
-
-        if figurka.startswith("pesiak"):
-            if figurka[6] == "B" and polickox == origposx and polickoy < origposy:
-                if polickoy >= origposy - 2 and bpesiak[int(figurka[7])] == 0:
-                    bpesiak[int(figurka[7])] = 1
+        if not check_posun(polickox, polickoy):
+            if figurka.startswith("pesiak"):
+                    if figurka[6] == "B" and polickox == origposx and polickoy < origposy:
+                        if polickoy >= origposy - 2 and bpesiak[int(figurka[7])] == 0:
+                            bpesiak[int(figurka[7])] = 1
+                            canvas.coords(figurka, x1, y1)
+                        elif polickoy >= origposy - 1:
+                            bpesiak[int(figurka[7])] = 1
+                            canvas.coords(figurka, x1, y1)
+                    elif figurka[6] == "C" and polickox == origposx and polickoy > origposy:
+                        if polickoy <= origposy + 2 and cpesiak[int(figurka[7])] == 0:
+                            cpesiak[int(figurka[7])] = 1
+                            canvas.coords(figurka, x1, y1)
+                        elif polickoy <= origposy + 1:
+                            cpesiak[int(figurka[7])] = 1
+                            canvas.coords(figurka, x1, y1)
+            if figurka.startswith("veza"):
+                if polickoy == origposy or polickox == origposx:
+                    (polickox != origposx and polickoy == origposy) or (origposx == polickox and origposy != polickoy)
                     canvas.coords(figurka, x1, y1)
-                elif polickoy >= origposy - 1:
-                    bpesiak[int(figurka[7])] = 1
+            if figurka.startswith("strelec"):
+                if abs(polickoy - origposy) == abs(polickox - origposx):
                     canvas.coords(figurka, x1, y1)
-            elif figurka[6] == "C" and polickox == origposx and polickoy > origposy:
-                if polickoy >= origposy + 2 and cpesiak[int(figurka[7])] == 0:
-                    cpesiak[int(figurka[7])] = 1
+            if figurka.startswith("kon"):
+                if((polickoy == origposy + 2 and (polickox == origposx + 1 or polickox == origposx - 1)) or
+                (polickoy == origposy - 2 and (polickox == origposx + 1 or polickox == origposx - 1)) or
+                (polickox == origposx + 2 and (polickoy == origposy + 1 or polickoy == origposy - 1)) or
+                (polickox == origposx - 2 and (polickoy == origposy + 1 or polickoy == origposy - 1))):
                     canvas.coords(figurka, x1, y1)
-                elif polickoy >= origposy + 1:
-                    cpesiak[int(figurka[7])] = 1
+            if figurka.startswith("kralovna"):
+                if((polickoy == origposy or polickox == origposx) or 
+                (abs(polickoy - origposy) == abs(polickox - origposx))):
                     canvas.coords(figurka, x1, y1)
-        if figurka.startswith("veza"):
-            if polickoy == origposy or polickox == origposx:
-                canvas.coords(figurka, x1, y1)
-        if figurka.startswith("strelec"):
-            if abs(polickoy - origposy) == abs(polickox - origposx):
-                canvas.coords(figurka, x1, y1)
-        if figurka.startswith("kon"):
-            if((polickoy == origposy + 2 and (polickox == origposx + 1 or polickox == origposx - 1)) or
-               (polickoy == origposy - 2 and (polickox == origposx + 1 or polickox == origposx - 1)) or
-               (polickox == origposx + 2 and (polickoy == origposy + 1 or polickoy == origposy - 1)) or
-               (polickox == origposx - 2 and (polickoy == origposy + 1 or polickoy == origposy - 1))):
-                canvas.coords(figurka, x1, y1)
-        if figurka.startswith("kralovna"):
-            if((polickoy == origposy or polickox == origposx) or 
-               (abs(polickoy - origposy) == abs(polickox - origposx))):
-                canvas.coords(figurka, x1, y1)
-        if figurka.startswith("kral"):
-            if((abs(polickoy - origposy) == 0) and (abs(polickox - origposx) == 1) or ((abs(polickoy - origposy) == 1) and (abs(polickox - origposx) == 0)) or ((abs(polickoy - origposy) == 1) and (abs(polickox - origposx) == 1))):
-                canvas.coords(figurka, x1, y1)
+            if figurka.startswith("kral"):
+                if((abs(polickoy - origposy) == 0) and (abs(polickox - origposx) == 1) or ((abs(polickoy - origposy) == 1) and (abs(polickox - origposx) == 0)) or ((abs(polickoy - origposy) == 1) and (abs(polickox - origposx) == 1))):
+                    canvas.coords(figurka, x1, y1)
 
         figurka = ""
+        biely = not biely
+        bol_vyber = False
 
 
 for i in range(9):
